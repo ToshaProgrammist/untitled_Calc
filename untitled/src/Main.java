1d0a244626f9be62e83432da30f73293ee980583
Anton Belenko <belenko_2002@mail.ru>
2025-01-26 19:05:02 +0600

    result

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,8 +7,6 @@ import Enum.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("HELLO GUYS");
-        System.out.println("HELLO GUYS 2");
-        System.out.println("HELLO GUYS 3");
         Scanner s = new Scanner(System.in);
         System.out.println("Введите пример согласно формату:\n" +
                 "(римское число/арабское) (операнд) (римское число/арабское)\n" +
